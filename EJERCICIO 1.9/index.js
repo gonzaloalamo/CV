55fab015b69fe562c491b940e072ca9a3a67fae7
Gonzalo Alamo <77199027+gonzaloalamo@users.noreply.github.com>
2021-01-29 08:04:34 +0000

    Delete index.js

--- EJERCICIO 1.9/index.js
+++
@@ -1,6 +0,0 @@
-const topics = ['JavaScript', 'Variables', 'funciones', 'condicionales', 'bucles'];
-
-const capitalizeTopics = topics.map(eachTopic => eachTopic.toUpperCase());
-capitalizeTopics.reverse();
-console. log('topics', topics);
-console. log('capitalizeTopics', capitalizeTopics);
